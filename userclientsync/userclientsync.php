dff0087e4b8a7fda13009db45b7c0f7382b09f7d
TwistedPretzel <127003436+TwistedPretzel@users.noreply.github.com>
2023-10-19 03:48:32 +0100

    Update userclientsync.php

--- userclientsync/userclientsync.php
+++ userclientsync/userclientsync.php
@@ -2,20 +2,14 @@
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
 }
-
 function userclientsync_config()
 {
     return [
-        // Display name for your module
         'name' => 'User/Client Sync',
-        // Description displayed within the admin interface
-        'description' => 'This module provides automaatic 2 way syncing between client and user details',
-        // Module author name
-        'author' => 'Twizel',
-        // Default language
+        'description' => 'Automatic 2 way syncing between client and user details',
+        'author' => 'ByteBlend',
         'language' => 'english',
-        // Version number
-        'version' => '1.1',
+        'version' => '1.2',
     ];
 }
 ?>
